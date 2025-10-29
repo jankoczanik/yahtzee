ccaa76306627c93226ddf54763e3590d9e6fb0a7
jankoczanik <jankojczanik@gmail.com>
2025-10-28 23:21:27 -0700

    created dice.h

---
+++ include/Dice.h
@@ -0,0 +1,16 @@
+#ifndef Dice_h
+#define Dice_h
+
+class Dice {
+
+    public:
+        Dice();
+        void roll();
+        int getFace() const;
+
+    private:
+
+        int face;
+};
+
+#endif
